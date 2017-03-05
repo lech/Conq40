3adcf776b61ebef6055e7053ebd04ea99203a897
Lech D <unattended@gmail.com>
2017-03-04 23:41:29 -0600

    Baseline Corrections
    - Correcting some baselines

--- srcds_launch.bat
+++ srcds_launch.bat
@@ -1,1 +1,1 @@
-srcds.exe -port 27015 +maxplayers 10 -console -noreplay -secure +exec server.cfg +sv_pure 0 +mp_coop_lobbysize 8 +map "drycanal conquer" - workshop
+srcds.exe -port 27015 +maxplayers 10 +exec server.cfg +sv_pure 0 +mp_coop_lobbysize 8 +map "drycanal conquer" -console -noreplay -secure -workshop
--- srcds_launch.sh
+++ srcds_launch.sh
@@ -1,1 +1,1 @@
-./wrapper.sh -game insurgency +maxplayers 10 -ip @SERVERIP -port @SERVERPORT -console -noreplay -secure +exec server.cfg +sv_pure 0 +map "drycanal conquer" -workshop
+./wrapper.sh -game insurgency +maxplayers 10 -ip @SERVERIP -port @SERVERPORT +exec server.cfg +sv_pure 0 +map "drycanal conquer" -console -noreplay -secure -workshop
