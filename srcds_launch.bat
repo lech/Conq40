29fcd57b8b1eca4ba018970b83601550f6681d76
Lech D <unattended@gmail.com>
2017-04-26 15:06:33 -0500

    Cleanup and corrections
    Just a small commit to correct some mistakes and extra spacing.

--- srcds_launch.bat
+++ srcds_launch.bat
@@ -1,1 +1,1 @@
-srcds.exe -port 27015 +maxplayers 10 +exec server.cfg +sv_pure 0 +mp_coop_lobbysize 8 +map "drycanal conquer" -console -noreplay -secure -workshop
+srcds.exe -port 27015 +maxplayers 10 +exec server.cfg +sv_pure 1 +mp_coop_lobbysize 8 +map "drycanal conquer" -console -noreplay -secure -workshop
--- srcds_launch.sh
+++ srcds_launch.sh
@@ -1,1 +1,1 @@
-./wrapper.sh -game insurgency +maxplayers 10 -ip @SERVERIP -port @SERVERPORT +exec server.cfg +sv_pure 0 +map "drycanal conquer" -console -noreplay -secure -workshop
+./wrapper.sh -game insurgency +maxplayers 10 -ip @SERVERIP -port @SERVERPORT +exec server.cfg +sv_pure 1 +map "drycanal conquer" -console -noreplay -secure -workshop
